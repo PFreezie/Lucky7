fc1010629c9b0bdcc1860e77cfb8bfd44781f206
PFreezie <147715183+PFreezie@users.noreply.github.com>
2024-10-24 16:31:35 +0300

    random numeroiden luonti ja rahamäärän syötön lisäys
    Käyttäjä voi nyt määrätä minkä verran rahaa laittaa peliin. Rahamäärä täytyy kuitenkin olla sen verran rahaa, mitä käyttäjällä on. Rahamäärä ei voi olla 0 tai vähemmän, sekä enemmän minkä verran rahaa on.
    
    Pelaaja voi voittaa 3 kertaa yhdessä pelissä. Ensimmäisen voiton kerroin on 300%, ja seuraavien voitto kertoimet nousevat +200%:lla. Voitot lisätään automaattisesti rahastoon, ja jos pelaaja haluaa jatkaa pelaamista, voitto kerroin nollaantuu heti ennen uuden rahamäärän syöttämistä.
    
    Jos rahaa on 0, peli sulkeutuu automaattisesti.

--- Lucky7 java/src/App.java
+++ Lucky7 java/src/App.java
@@ -1,17 +1,80 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
         int raha = 5;
+        int voitto = 0;
+        String pelata = "temporary";
         String jatkaa = "";
 
         Scanner in = new Scanner(System.in);
+        Random Random = new Random();
 
-        System.out.println("Lisää rahat peliin. Omistat "+raha+"€ yhteensä rahaa!");
 
         do {
+            voitto = 0;
+            System.out.println("Lisää rahat peliin. Omistat "+raha+" euroa yhteensä rahaa!");
+            int rahaMaara = Integer.parseInt(in.nextLine());
 
+            do {
+                if ( rahaMaara > raha){
+                    System.out.println("Sinä et voi laittaa tuon verran rahaa");
+                    System.out.println("Lisää rahat peliin. Omistat "+raha+" euroa yhteensä rahaa!");
+                    rahaMaara = Integer.parseInt(in.nextLine());
+                }
+                else if (rahaMaara <= 0) {
+                    System.out.println("Sinä et voi laittaa tuon verran rahaa");
+                    System.out.println("Lisää rahat peliin. Omistat "+raha+" euroa yhteensä rahaa!");
+                    rahaMaara = Integer.parseInt(in.nextLine());
+                }
+                else {
+                    System.out.println("Paina enter pelataksesi peliä. Jos et halua pelata, paina e ja Enter!");
+                    pelata = in.nextLine();
+                }
+                
+            } while (!pelata.equals("") && !pelata.equals("e"));
+
+            if (pelata.equals("e")) {
+                break;
+            }
+            else {
+                raha -= rahaMaara;
+            }
+            
+            
+            int randomNumber1 = Random.nextInt(7)+1;
+            int randomNumber2 = Random.nextInt(7)+1;
+            int randomNumber3 = Random.nextInt(7)+1;
+
+            if (randomNumber1 == 7 && randomNumber2 == 7 && randomNumber3 == 7) {
+                voitto +=3;
+                System.out.println("Voitit 3 kertaa!");
+                raha += (rahaMaara*7);
+                System.out.println("Omistat "+raha+" euroa yhteensä rahaa!");
+            }
+            else if (randomNumber1 == 7 && randomNumber2 == 7 || randomNumber2 == 7 && randomNumber3 == 7 || randomNumber1 == 7 && randomNumber3 == 7){
+                voitto +=2;
+                System.out.println("Voitit 2 kertaa!");
+                System.out.println("Omistat "+raha+" euroa yhteensä rahaa!");
+                raha += (rahaMaara*5);
+            }
+            else if (randomNumber1 == 7 || randomNumber2 == 7 || randomNumber3 == 7){
+                voitto +=1;
+                raha += (rahaMaara*3);
+                System.out.println("Voitit!");
+                System.out.println("Omistat "+raha+" euroa yhteensä rahaa!");
+            }
+            else {
+                System.out.println("Hävisit!");
+                System.out.println("Omistat "+raha+" euroa yhteensä rahaa!");
+
+                if (raha <= 0){
+                    System.out.println("Sinulla ei ole rahaa. Et voi enää pelata!");
+                    break;
+                }
+            }
 
             System.out.println("Haluatko jatkaa? Paina Enter jatkaaksesi! Poistu pelistä painamalla e ja Enter.");
             jatkaa = in.nextLine();
