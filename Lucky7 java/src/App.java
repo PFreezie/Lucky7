ec10f1a5006a4f14d36152b766ce3e0ca1a1ca3a
PFreezie <147715183+PFreezie@users.noreply.github.com>
2024-10-24 14:53:25 +0300

    Ehtolauseen luonti.
    Tässä vaiheessa käyttäjä voi lisätä mitä tahansa inputtiin ja sen myötä ohjelma kaatuu.

---
+++ Lucky7 java/src/App.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int raha = 5;
+        String jatkaa = "";
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Lisää rahat peliin. Omistat "+raha+"€ yhteensä rahaa!");
+
+        do {
+
+
+            System.out.println("Haluatko jatkaa? /n Paina Enter jatkaaksesi! Poistu pelistä painamalla e ja Enter.");
+            jatkaa = in.nextLine();
+        } while ( jatkaa.equals(""));
+    }
+}
